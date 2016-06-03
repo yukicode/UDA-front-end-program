0f31793926f041bbf5c025768f41009d17dc19e9
yukicode <yolandaapply@gmail.com>
2016-06-02 17:29:57 -0700

    adding different sizes of images images are chosen based on screen size and view port

--- responsive-img/Project-Part-1-Start/image-resize.bat
+++ responsive-img/Project-Part-1-Start/image-resize.bat
@@ -1,2 +1,4 @@
 @ECHO OFF
-for %%X in (./images_src/*.jpg) do convert ./images_src/%%X -resize 1024 ./images/%%X 
+for %%X in (./images_src/*.jpg) do convert ./images_src/%%X -resize 1024 ./images/1240/%%X 
+for %%Y in (./images_src/*.jpg) do convert ./images_src/%%Y -resize 640 ./images/640/%%Y
+for %%Z in (./images_src/*.jpg) do convert ./images_src/%%Z -resize 320 ./images/320/%%Z 

d356728fa9feedf8843eb560a93302882560ce94
yukicode <yolandaapply@gmail.com>
2016-07-08 13:12:08 -0700

    add visual studio environment, add jasmine for unit test

---
+++ frogger-game/js/tests/test.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="../jasmine/jasmine.js" />
+/// <reference path="../app.js" />
+
+//describe("testing a simple function", function () {
+//    it("adds two numbers", function () {
+//        expect(add(1, 2)).toEqual(3);
+//    });
+//});

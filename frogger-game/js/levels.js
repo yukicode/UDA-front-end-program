33ce643f461bcb8be6e66498049277e02f44c24c
yukicode <yolandaapply@gmail.com>
2016-08-01 18:38:09 -0700

    modify formatting of levels

--- frogger-game/js/levels.js
+++ frogger-game/js/levels.js
@@ -5,7 +5,7 @@
         "colWidth": 101,
         "rowHeight": 83,
         "rowContent": ["water", "paved", "paved", "paved", "grass", "grass", ],
-        "enemySpawningRows": [2,3,4],
+        "enemySpawningRows": [2, 3, 4],
     },
     enemies: {
         "type": "bug",
@@ -24,7 +24,13 @@
         "content": "The Star",
     },
     countDownTime: 20,
-    levelCompleteTiles: [[1,2], [2,2], [3,2], [4,2], [5,2]],
+    levelCompleteTiles: [
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+        [5, 2]
+    ],
 };
 
 var level2 = {
@@ -34,7 +40,7 @@ var level2 = {
         "colWidth": 101,
         "rowHeight": 83,
         "rowContent": ["water", "paved", "paved", "paved", "paved", "grass"],
-        "enemySpawningRows": [2,4,5],
+        "enemySpawningRows": [2, 4, 5],
     },
     enemies: {
         "type": "bug",
@@ -48,23 +54,30 @@ var level2 = {
         "initCol": 4,
         "initRow": 4,
     },
-    rocks: [[1, 3, true], [3, 3], [4, 3], [5, 3]],
+    rocks: [
+        [1, 3, true],
+        [3, 3],
+        [4, 3],
+        [5, 3]
+    ],
     title: {
         "number": 2,
         "content": "The Rock",
     },
     countDownTime: 30,
-    levelCompleteTiles: [[4,2]],
+    levelCompleteTiles: [
+        [4, 2]
+    ],
 };
 
 var level3 = {
-    map:{
+    map: {
         "col": 7,
         "row": 8,
         "colWidth": 101,
         "rowHeight": 83,
         "rowContent": ["water", "paved", "paved", "paved", "paved", "paved", "paved", "grass", ],
-        "enemySpawningRows": [2,3,4,5,6,7],
+        "enemySpawningRows": [2, 3, 4, 5, 6, 7],
     },
     enemies: {
         "type": "bug",
@@ -82,14 +95,25 @@ var level3 = {
         "initCol": 5,
         "initRow": 5,
     },
-    rocks: [[2, 2, true], [4, 2], [3, 3, true], [5, 4, true], [2, 5, true], [4, 5], [5, 6, true],
-            [6, 7, true], [7, 8, true]],
+    rocks: [
+        [2, 2, true],
+        [4, 2],
+        [3, 3, true],
+        [5, 4, true],
+        [2, 5, true],
+        [4, 5],
+        [5, 6, true],
+        [6, 7, true],
+        [7, 8, true]
+    ],
     title: {
         "number": 3,
         "content": "The Key",
     },
     countDownTime: 30,
-    levelCompleteTiles: [[4,2]],
+    levelCompleteTiles: [
+        [4, 2]
+    ],
 };
 
 var level4 = {
@@ -99,7 +123,7 @@ var level4 = {
         "colWidth": 101,
         "rowHeight": 83,
         "rowContent": ["water", "paved", "paved", "paved", "paved", "paved", "paved", "grass", ],
-        "enemySpawningRows": [2,3,4,5,6],
+        "enemySpawningRows": [2, 3, 4, 5, 6],
     },
     enemies: {
         "type": "bug",
@@ -113,26 +137,46 @@ var level4 = {
         "initCol": 1,
         "initRow": 8,
     },
-    gems: [
-        {
-            "initCol": 2,
-            "initRow": 7,
-        },
-        {
-            "initCol": 3,
-            "initRow": 3,
-        },
-    ],
+    gems: [{
+        "initCol": 2,
+        "initRow": 7,
+    }, {
+        "initCol": 3,
+        "initRow": 3,
+    }, ],
     key: {
         "initCol": 7,
         "initRow": 2,
     },
-    rocks: [[6, 2, true], [2, 3, true],[4, 3], [6, 3], [4, 4, true], [6, 4], [4, 5, true], [6, 5],
-            [4, 6, true], [1, 7, true],　[4, 7],  [5, 7], [6, 7], [7, 7], [2, 8, true]],
+    rocks: [
+        [6, 2, true],
+        [2, 3, true],
+        [4, 3],
+        [6, 3],
+        [4, 4, true],
+        [6, 4],
+        [4, 5, true],
+        [6, 5],
+        [4, 6, true],
+        [1, 7, true], 　
+        [4, 7],
+        [5, 7],
+        [6, 7],
+        [7, 7],
+        [2, 8, true]
+    ],
     title: {
         "number": 4,
         "content": "The Time Gem",
     },
     countDownTime: 10,
-    levelCompleteTiles: [[1,2], [2,2], [3,2], [4,2], [5,2], [6,2], [7,2]],
+    levelCompleteTiles: [
+        [1, 2],
+        [2, 2],
+        [3, 2],
+        [4, 2],
+        [5, 2],
+        [6, 2],
+        [7, 2]
+    ],
 };

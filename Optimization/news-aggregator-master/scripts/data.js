b4dc9a0ea018bde766a6b1bb32d38b6c556ddd6f
yukicode <yolandaapply@gmail.com>
2016-08-05 23:04:19 -0700

    add files for server and work flow, beautified html and js files

--- Optimization/news-aggregator-master/scripts/data.js
+++ Optimization/news-aggregator-master/scripts/data.js
@@ -16,46 +16,46 @@
  */
 APP.Data = (function() {
 
-  var HN_API_BASE = 'https://hacker-news.firebaseio.com';
-  var HN_TOPSTORIES_URL = HN_API_BASE + '/v0/topstories.json';
-  var HN_STORYDETAILS_URL = HN_API_BASE + '/v0/item/[ID].json';
+    var HN_API_BASE = 'https://hacker-news.firebaseio.com';
+    var HN_TOPSTORIES_URL = HN_API_BASE + '/v0/topstories.json';
+    var HN_STORYDETAILS_URL = HN_API_BASE + '/v0/item/[ID].json';
 
-  function getTopStories(callback) {
-    request(HN_TOPSTORIES_URL, function(evt) {
-      callback(evt.target.response);
-    });
-  }
+    function getTopStories(callback) {
+        request(HN_TOPSTORIES_URL, function(evt) {
+            callback(evt.target.response);
+        });
+    }
 
-  function getStoryById(id, callback) {
+    function getStoryById(id, callback) {
 
-    var storyURL = HN_STORYDETAILS_URL.replace(/\[ID\]/, id);
+        var storyURL = HN_STORYDETAILS_URL.replace(/\[ID\]/, id);
 
-    request(storyURL, function(evt) {
-      callback(evt.target.response);
-    });
-  }
+        request(storyURL, function(evt) {
+            callback(evt.target.response);
+        });
+    }
 
-  function getStoryComment(id, callback) {
+    function getStoryComment(id, callback) {
 
-    var storyCommentURL = HN_STORYDETAILS_URL.replace(/\[ID\]/, id);
+        var storyCommentURL = HN_STORYDETAILS_URL.replace(/\[ID\]/, id);
 
-    request(storyCommentURL, function(evt) {
-      callback(evt.target.response);
-    });
-  }
+        request(storyCommentURL, function(evt) {
+            callback(evt.target.response);
+        });
+    }
 
-  function request(url, callback) {
-    var xhr = new XMLHttpRequest();
-    xhr.open('GET', url, true);
-    xhr.responseType = 'json';
-    xhr.onload = callback;
-    xhr.send();
-  }
+    function request(url, callback) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', url, true);
+        xhr.responseType = 'json';
+        xhr.onload = callback;
+        xhr.send();
+    }
 
-  return {
-    getTopStories: getTopStories,
-    getStoryById: getStoryById,
-    getStoryComment: getStoryComment
-  };
+    return {
+        getTopStories: getTopStories,
+        getStoryById: getStoryById,
+        getStoryComment: getStoryComment
+    };
 
 })();

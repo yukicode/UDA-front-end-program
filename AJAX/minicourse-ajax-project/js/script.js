8646610fdc9a7508e4fc24506a637c2ab1c5a43c
yukicode <yolandaapply@gmail.com>
2016-08-22 16:19:31 -0700

    add background image, use ny times API to load city news

--- AJAX/minicourse-ajax-project/js/script.js
+++ AJAX/minicourse-ajax-project/js/script.js
@@ -7,14 +7,44 @@ function loadData() {
     var $nytElem = $('#nytimes-articles');
     var $greeting = $('#greeting');
 
+    var street = $.trim($("#street").val());
+    var city = $.trim($("#city").val());
+    var src = "http://maps.googleapis.com/maps/api/streetview?size=600x300&location=" + street + ", " + city;
+    var imgContent = '<img class="bgimg" src="%src%"></img>';
+    var $img = $(".bgimg");
     // clear out old data before new request
     $wikiElem.text("");
     $nytElem.text("");
 
     // load streetview
-
-    // YOUR CODE GOES HERE!
-
+    if($img.length){
+        $img.attr("src", src);
+    }else{
+        $body.append(imgContent.replace("%src%", src));
+    }
+    // send request to nytimes
+    var url = "https://api.nytimes.com/svc/search/v2/articlesearch.json";
+    var temp = '<li class="article"><a href="%link%">%title%</a><p>%content%</p></li>';
+    var fragment = "";
+    url += '?' + $.param({
+        'q': city,
+        'sort': "newest",
+    });
+    $.getJSON(url, function(data){
+        console.log(data.response.docs);
+        $.each(data.response.docs,function(index, val){
+            if(val.snippet){
+                fragment += temp.replace("%link%", val.web_url).replace("%title%", val.headline.main)
+                            .replace("%content%", val.snippet);
+            }else{
+                fragment += temp.replace("%link%", val.web_url).replace("%title%", val.headline.main)
+                            .replace("%content%", "");
+            }
+            
+        });
+        fragment = "<ul>" + fragment + "</ul>";
+        $body.append(fragment);
+    });
     return false;
 };
 

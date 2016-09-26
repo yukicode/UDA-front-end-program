db3f5dc268e34d316424181e0f1ecffe45fb0773
yukicode <yolandaapply@gmail.com>
2016-09-25 23:23:40 -0700

    modify gulp and change source/build files, add readme

--- Neighborhood-map/gulpfile.js
+++ Neighborhood-map/gulpfile.js
@@ -7,58 +7,58 @@ var jsMini = require("gulp-uglify");
 var cssMini = require("gulp-clean-css");
 
 //////////////////////////////////////////////////////////
-//Public Build
+//Dist Build
 //////////////////////////////////////////////////////////
 
-//empty public folder
+//empty dist folder
 gulp.task("delete", function() {
     del.sync([
-        "./public/**",
+        "./dist/**",
     ]);
 });
 
-//copy everything to the public folder
+//copy everything to the dist folder
 gulp.task("copy", ["delete"], function() {
-    return gulp.src(["./app/**/*"])
-        .pipe(gulp.dest("./public"));
+    return gulp.src(["./src/**/*"])
+        .pipe(gulp.dest("./dist"));
 });
 
 //delete files that will be optimized separately and delete empty folders
 gulp.task("clean", ["copy"], function() {
     del.sync([
-        "./public/**/*.js",
-        "./public/**/*.css",
-        "./public/**/*.html",
+        "./dist/**/*.js",
+        "./dist/**/*.css",
+        "./dist/**/*.html",
     ]);
-    deleteEmpty.sync("./public/");
+    deleteEmpty.sync("./dist/");
 });
 
 //inline resources for index.html, and minify html
 gulp.task("build:index", ["clean"], function() {
-    return gulp.src(["./app/index.html"])
+    return gulp.src(["./src/index.html"])
         .pipe(inline())
         .pipe(htmlMini({
             collapseWhitespace: true
         }))
-        .pipe(gulp.dest("./public/"));
+        .pipe(gulp.dest("./dist/"));
 });
 
 //minify js files that are not inline
 gulp.task("build:js", ["clean"], function() {
-    return gulp.src(["./app/js/!(*.inline.js)"])
+    return gulp.src(["./src/js/!(*.inline.js)"])
         .pipe(jsMini())
-        .pipe(gulp.dest("./public/js"));
+        .pipe(gulp.dest("./dist/js"));
 });
 
 //minify css files that are not inline
 gulp.task("build:css", ["clean"], function() {
-    return gulp.src("./app/css/!(*.inline.css)")
+    return gulp.src("./src/css/!(*.inline.css)")
         .pipe(cssMini({
             compatibility: "ie8"
         }))
-        .pipe(gulp.dest("./public/css"));
+        .pipe(gulp.dest("./dist/css"));
 });
 
 
-//build for public directory
+//build for dist directory
 gulp.task("build", ["build:js", "build:css", "build:index"]);
---
+++ Neighborhood-map/readme.md
@@ -0,0 +1,25 @@
+# Neighborhood-map
+An application that helps you find apartments near Seattle, WA.
+This app uses Google Map API and Yelp API.
+The source files are in src folder, the minified files are in the dist folder.
+
+### Install
+1. Make sure that you have node.js in the system. You can download the newest version here [Node](https://nodejs.org/en/).
+2. Download and unzip this repo.
+3. In command line, go to the directory of the downloaded file and type `npm install`. This will install all the dependencies needed to successfully run the app.
+4. Type `node app` to run the local server. It will server the minified webpage at port 3000.
+5. In browser, go to `localhost:3000` to use to application.
+
+### Features
+* Displays all the apartments near Seattle area by default.
+* Display apartment names, phone numbers, websites, price ranges, yelp reviews, google reviews and thumbnail images.
+* Search apartments by name.
+* Search apartments near by desired locations.
+* Sort apartments by name or price.
+
+### Todo
+[] Link with database.
+[] Add favorite button.
+[] Add favorite menu that displays all the favorite apartments's details.
+[] Add local storage of favorites.
+
